2b88ab824f61a4e4cbe320a989001bac4524764c
S4vianR <reuben4.com@gmail.com>
2025-05-28 15:34:47 -0600

    Commit

--- ReproductorBuzzer/Form1.Designer.cs
+++ ReproductorBuzzer/Form1.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.reproducirButton = new System.Windows.Forms.Button();
+            this.pausarButton = new System.Windows.Forms.Button();
+            this.conectarButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -41,7 +43,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(200, 16);
+            this.label1.Location = new System.Drawing.Point(38, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(360, 29);
             this.label1.TabIndex = 1;
@@ -49,47 +51,71 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.conectarButton);
             this.panel1.Controls.Add(this.statusTextBox);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 331);
+            this.panel1.Size = new System.Drawing.Size(416, 331);
             this.panel1.TabIndex = 2;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(27, 80);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(166, 80);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "La cucaracha";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(17, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Reproducir";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // statusTextBox
             // 
-            this.statusTextBox.Location = new System.Drawing.Point(27, 275);
+            this.statusTextBox.Location = new System.Drawing.Point(44, 273);
             this.statusTextBox.Name = "statusTextBox";
             this.statusTextBox.Size = new System.Drawing.Size(314, 21);
             this.statusTextBox.TabIndex = 3;
             this.statusTextBox.Text = "No se está reproduciendo nada por el momento";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pausarButton);
+            this.groupBox1.Controls.Add(this.reproducirButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(44, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 80);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nokia";
+            // 
+            // reproducirButton
+            // 
+            this.reproducirButton.Location = new System.Drawing.Point(17, 27);
+            this.reproducirButton.Name = "reproducirButton";
+            this.reproducirButton.Size = new System.Drawing.Size(129, 37);
+            this.reproducirButton.TabIndex = 1;
+            this.reproducirButton.Text = "Reproducir";
+            this.reproducirButton.UseVisualStyleBackColor = true;
+            this.reproducirButton.Click += new System.EventHandler(this.reproducirButton_Click);
+            // 
+            // pausarButton
+            // 
+            this.pausarButton.Location = new System.Drawing.Point(178, 27);
+            this.pausarButton.Name = "pausarButton";
+            this.pausarButton.Size = new System.Drawing.Size(129, 37);
+            this.pausarButton.TabIndex = 2;
+            this.pausarButton.Text = "Pausar";
+            this.pausarButton.UseVisualStyleBackColor = true;
+            this.pausarButton.Click += new System.EventHandler(this.pausarButton_Click);
+            // 
+            // conectarButton
+            // 
+            this.conectarButton.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conectarButton.Location = new System.Drawing.Point(120, 230);
+            this.conectarButton.Name = "conectarButton";
+            this.conectarButton.Size = new System.Drawing.Size(152, 37);
+            this.conectarButton.TabIndex = 3;
+            this.conectarButton.Text = "Conectar arduino";
+            this.conectarButton.UseVisualStyleBackColor = true;
+            this.conectarButton.Click += new System.EventHandler(this.conectarButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 356);
+            this.ClientSize = new System.Drawing.Size(440, 356);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
@@ -107,7 +133,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox statusTextBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button reproducirButton;
+        private System.Windows.Forms.Button pausarButton;
+        private System.Windows.Forms.Button conectarButton;
     }
 }
 
--- ReproductorBuzzer/Form1.cs
+++ ReproductorBuzzer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports; // Esta directiva using System.IO.Ports; estaba duplicada, la eliminé
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,168 @@ namespace ReproductorBuzzer
 {
     public partial class Form1 : Form
     {
+        SerialPort arduinoPort;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); // Solo debe llamarse una vez
             statusTextBox.Text = "No se está reproduciendo nada por el momento";
+
+            arduinoPort = new SerialPort();
+            arduinoPort.BaudRate = 9600; // La velocidad debe coincidir con la de Arduino (Serial.begin(9600))
+            arduinoPort.PortName = "COM5"; // Establecemos COM5 como el puerto predeterminado
+            arduinoPort.DataReceived += ArduinoPort_DataReceived; // Suscribe el evento para cuando Arduino envía datos
+
+            // Deshabilitar botones de control de melodía hasta que estemos conectados
+            reproducirButton.Enabled = false;
+            pausarButton.Enabled = false;
+
+            // Al iniciar, intentamos conectar automáticamente al COM5
+            // Puedes decidir si quieres que se conecte automáticamente o requiera un botón "Conectar"
+            // Si quieres auto-conexión, podrías llamar a una función ConnectPort() aquí.
+            // Para mantener la consistencia con el código anterior que tenía un botón "Conectar",
+            // lo dejaremos manual, asumiendo que tienes un botón `btnConnect` en tu formulario
+            // aunque no esté en el código que mostraste. Si no tienes un botón de conexión,
+            // considera añadir la lógica de conexión aquí.
+        }
+
+        // Si no tienes un botón "Conectar" y quieres autoconectar,
+        // podrías añadir esta lógica en el constructor o en Form_Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Opcional: Si quieres que se intente conectar automáticamente al cargar el formulario
+            // Sin un botón de conexión explícito, esto hace que la aplicación intente conectarse al inicio.
+            // Si tienes un botón de conexión, este método podría quedar vacío o eliminarse.
+            // IntentarConectarArduino(); // Llama a una función para intentar la conexión
+        }
+
+        /*
+         * Si quieres un botón "Conectar" separado, como en la versión anterior,
+         * asegúrate de que el botón `btnConnect` esté en tu formulario y su evento
+         * `Click` esté vinculado a este método.
+         */
+        private void conectarButton_Click(object sender, EventArgs e)
+        {
+            if (arduinoPort.IsOpen)
+            {
+                try
+                {
+                    arduinoPort.Close();
+                    // Asumiendo que btnConnect existe y es el botón de conexión/desconexión
+                    // btnConnect.Text = "Conectar";
+                    reproducirButton.Enabled = false;
+                    pausarButton.Enabled = false;
+                    statusTextBox.AppendText("Desconectado del puerto serial." + Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    statusTextBox.AppendText("Error al desconectar: " + ex.Message + Environment.NewLine);
+                }
+            }
+            else
+            {
+                // La lógica de conexión se ha movido a un método separado para posible reuso
+                AttemptConnectArduino();
+            }
+        }
+
+        // Método auxiliar para intentar la conexión a Arduino
+        private void AttemptConnectArduino()
+        {
+            try
+            {
+                arduinoPort.Open();
+                // Asumiendo que btnConnect existe
+                // btnConnect.Text = "Desconectar";
+                reproducirButton.Enabled = true;
+                pausarButton.Enabled = true;
+                statusTextBox.AppendText("Conectado a " + arduinoPort.PortName + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                statusTextBox.AppendText("Error al conectar al COM5: " + ex.Message + Environment.NewLine);
+                reproducirButton.Enabled = false;
+                pausarButton.Enabled = false;
+            }
+        }
+
+
+        // Evento que se dispara cuando Arduino envía datos a través del puerto serial
+        private void ArduinoPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = arduinoPort.ReadLine(); // Lee una línea completa de datos del Arduino (hasta un '\n')
+
+                // Es CRUCIAL usar Invoke para actualizar la UI desde un hilo secundario
+                // El evento DataReceived se ejecuta en un hilo diferente al de la UI principal.
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    statusTextBox.AppendText("Arduino: " + data + Environment.NewLine);
+                    // Opcional: Auto-scroll al final del TextBox
+                    statusTextBox.SelectionStart = statusTextBox.Text.Length;
+                    statusTextBox.ScrollToCaret();
+                }));
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores durante la lectura (ej. Arduino desconectado inesperadamente)
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    statusTextBox.AppendText("Error de lectura: " + ex.Message + Environment.NewLine);
+                }));
+            }
+        }
+
+        private void reproducirButton_Click(object sender, EventArgs e)
+        {
+            if (arduinoPort.IsOpen)
+            {
+                try
+                {
+                    arduinoPort.Write("P"); // Envía el comando 'P' a Arduino (Play)
+                    statusTextBox.AppendText("Comando 'P' enviado: Iniciar melodía." + Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    statusTextBox.AppendText("Error al enviar comando 'P': " + ex.Message + Environment.NewLine);
+                }
+            }
+            else
+            {
+                statusTextBox.AppendText("No conectado a Arduino. Por favor, intenta conectar." + Environment.NewLine);
+                AttemptConnectArduino(); // Intenta reconectar si no está conectado
+            }
+        }
+
+        private void pausarButton_Click(object sender, EventArgs e)
+        {
+            if (arduinoPort.IsOpen)
+            {
+                try
+                {
+                    arduinoPort.Write("S"); // Envía el comando 'S' a Arduino (Stop)
+                    statusTextBox.AppendText("Comando 'S' enviado: Detener melodía." + Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    statusTextBox.AppendText("Error al enviar comando 'S': " + ex.Message + Environment.NewLine);
+                }
+            }
+            else
+            {
+                statusTextBox.AppendText("No conectado a Arduino. Por favor, intenta conectar." + Environment.NewLine);
+                AttemptConnectArduino(); // Intenta reconectar si no está conectado
+            }
+        }
+
+        // Se ejecuta cuando el formulario se está cerrando
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (arduinoPort.IsOpen)
+            {
+                arduinoPort.Close(); // Asegúrate de cerrar el puerto serial al cerrar la aplicación
+            }
         }
     }
 }
